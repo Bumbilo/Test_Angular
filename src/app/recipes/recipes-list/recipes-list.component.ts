050a538de9758f189c24ee8af14d4726e1c02f41
Dmitry Holiak <dimagolya@gmail.com>
2018-12-05 16:55:52 +0200

    test commit 2

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -10,7 +10,9 @@ import { RecipeService } from '../recipe.service';
 export class RecipesListComponent implements OnInit {
   recipes: Recipe[];
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService) { 
+  
+  }
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
